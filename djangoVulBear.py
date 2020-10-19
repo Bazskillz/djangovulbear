00e3cf89e6e80836b1ade31143b6545d389705da
Bazskillz <bas-daniel2@hotmail.com>
2020-10-19 21:00:25 +0200

    Check for MD5 function

--- djangoVulBear.py
+++ djangoVulBear.py
@@ -6,7 +6,7 @@ class DjanogoVulBear(LocalBear):
     def run(self, filename, file):
         """
         DjangoVulBear
-        Checks your django project for common misconfigurations and bugs.
+        Checks your django project for common misconfigurations and vulnerable implementation.
 
         :param None:
         """
@@ -18,7 +18,7 @@ class DjanogoVulBear(LocalBear):
             for line in file:
                 if str.lower(line) in debug_misconfigurations:
                     yield self.new_result(message="Are you running this django project in production?"
-                                                  " If so, please disable debug options" + line, file=filename)
+                                                  " If so, please disable debug options > " + line, file=filename)
             # CookieStorage option in settings.py
             for line in file:
                 if "cookie.CookieStorage" in line:
@@ -26,3 +26,8 @@ class DjanogoVulBear(LocalBear):
                                                   " users to read any session data, and if your SECRET_KEY is "
                                                   "compromised they can also manipulate session data. Sessions can be "
                                                   "implemented using the session module.", file=filename)
+            for line in file:
+                if "MD5PasswordHasher" in line:
+                    yield self.new_result(message="Are you using MD5 for as a password hashing function? Please refrain"
+                                                  " from using MD5 for password storage, instead use something"
+                                                  " like Argon2 or BCrypt", file=filename)
